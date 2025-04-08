e23a898b785016f67488332fb679358de454e7cb
Davide Testuggine <davidet@fb.com>
2025-04-08 14:41:11 -0700

    Factor out param server

--- torchtune/dev/rl/workers/__init__.py
+++ torchtune/dev/rl/workers/__init__.py
@@ -1,9 +1,5 @@
 from .datacollectors import SyncLLMCollector
 from .metric_logger import MetricLoggerActor
+from .parameter_servers import vLLMParameterServer
 from .ref_actor import RefActor
 from .trainers import PyTorchActorModel
-from .weight_updaters import (
-    stateless_init_process_group,
-    vLLMHFWeightUpdateReceiver,
-    vLLMParameterServer,
-)
