5538ae3f15d0cd460d404a8135a8699d84d0195e
vmoens <vmoens@meta.com>
2025-04-25 07:53:13 -0700

    amend

--- torchtune/dev/rl/workers/datacollectors/sync.py
+++ torchtune/dev/rl/workers/datacollectors/sync.py
@@ -16,8 +16,8 @@ from tensordict import lazy_stack, TensorDictBase
 from torchdata.stateful_dataloader import StatefulDataLoader
 from torchdata.stateful_dataloader.sampler import StatefulDistributedSampler
 from torchrl.collectors import (
-    LocalWeightUpdaterBase,
-    RemoteWeightUpdaterBase,
+    WeightUpdateSenderBase as LocalWeightUpdaterBase,
+    WeightUpdateReceiverBase as RemoteWeightUpdaterBase,
     SyncDataCollector,
 )
 
--- torchtune/dev/rl/workers/parameter_servers/vllm.py
+++ torchtune/dev/rl/workers/parameter_servers/vllm.py
@@ -2,8 +2,8 @@ import ray
 import torch
 from readerwriterlock import rwlock
 from torchrl.collectors.collectors import (
-    LocalWeightUpdaterBase,
-    RemoteWeightUpdaterBase,
+    WeightUpdateSenderBase as LocalWeightUpdaterBase,
+    WeightUpdateReceiverBase as RemoteWeightUpdaterBase,
 )
 from torchtune.dev.rl.utils import stateless_init_process_group
 from torchtune.utils import get_logger
